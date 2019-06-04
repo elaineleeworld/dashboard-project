ea2ab42e74161f8859084587c23c25e35c574bff
Elaine Lee <elaine.lee@ff0000.com>
2019-06-03 21:07:27 -0700

    add material ui icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./App.css";
 import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
-import CalendarTodayRounded from "@material-ui/icons/CalendarTodayRounded";
+// import CalendarTodayRounded from "@material-ui/icons/CalendarTodayRounded";
+import {
+  CalendarToday,
+  Description,
+  Home,
+  Whatshot,
+  Beenhere,
+  ViewCompact,
+  InsertChartOutlined,
+  AttachMoney,
+  ViewHeadline,
+  Note,
+  DataUsage,
+  Memory,
+  DonutLarge
+} from "@material-ui/icons";
 
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 
@@ -18,85 +33,85 @@ function App() {
         <SideNav.Nav defaultSelected="calendar">
           <NavItem eventKey="calendar">
             <NavIcon>
-              <CalendarTodayRounded style={{ fontSize: "1em", color: "#7F96BA" }} />
+              <CalendarToday style={{ fontSize: "1em", color: "#7F96BA" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>Calendar</NavText>
           </NavItem>
           <NavItem eventKey="documentation">
             <NavIcon>
-              <i className="fa fa-fw fa-line-chart" style={{ fontSize: "1.75em" }} />
+              <Description style={{ fontSize: "1.1em", color: "#7F96BA" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>Documentation</NavText>
           </NavItem>
           <NavItem eventKey="dashboard">
             <NavIcon>
-              <i className="fa fa-fw fa-home" style={{ fontSize: "1.75em" }} />
+              <Home style={{ fontSize: "1.1em", color: "#7F96BA" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>Dashboard</NavText>
           </NavItem>
           <NavItem eventKey="admin plugins">
             <NavIcon>
-              <i className="fa fa-fw fa-line-chart" style={{ fontSize: "1.75em" }} />
+              <Whatshot style={{ fontSize: "1.1em", color: "#7F96BA" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>Admin Plugins</NavText>
           </NavItem>
           <NavItem eventKey="admin forms">
             <NavIcon>
-              <i className="fa fa-fw fa-line-chart" style={{ fontSize: "1.75em" }} />
+              <Beenhere style={{ fontSize: "1.1em", color: "#7F96BA" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>Admin Forms</NavText>
           </NavItem>
           <NavItem eventKey="admin layouts">
             <NavIcon>
-              <i className="fa fa-fw fa-line-chart" style={{ fontSize: "1.75em" }} />
+              <ViewCompact style={{ fontSize: "1.1em", color: "#7F96BA" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>Admin Layouts</NavText>
           </NavItem>
           <NavItem eventKey="information panels">
             <NavIcon>
-              <i className="fa fa-fw fa-line-chart" style={{ fontSize: "1.75em" }} />
+              <InsertChartOutlined style={{ fontSize: "1.1em", color: "#7F96BA" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>Information Panels</NavText>
           </NavItem>
           <NavItem eventKey="ecommerce">
             <NavIcon>
-              <i className="fa fa-fw fa-line-chart" style={{ fontSize: "1.75em" }} />
+              <AttachMoney style={{ fontSize: "1.1em", color: "#7F96BA" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>Ecommerce</NavText>
           </NavItem>
           <NavItem eventKey="ui elements">
             <NavIcon>
-              <i className="fa fa-fw fa-home" style={{ fontSize: "1.75em" }} />
+              <Memory style={{ fontSize: "1.1em", color: "#7F96BA" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>UI Elements</NavText>
           </NavItem>
           <NavItem eventKey="form elements">
             <NavIcon>
-              <i className="fa fa-fw fa-line-chart" style={{ fontSize: "1.75em" }} />
+              <ViewHeadline style={{ fontSize: "1.1em", color: "#7F96BA" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>Form Elements</NavText>
           </NavItem>
           <NavItem eventKey="plugins">
             <NavIcon>
-              <i className="fa fa-fw fa-line-chart" style={{ fontSize: "1.75em" }} />
+              <DataUsage style={{ fontSize: "1.1em", color: "#7F96BA" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>Plugins</NavText>
           </NavItem>
           <NavItem eventKey="pages">
             <NavIcon>
-              <i className="fa fa-fw fa-line-chart" style={{ fontSize: "1.75em" }} />
+              <Note style={{ fontSize: "1.1em", color: "#7F96BA" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>Pages</NavText>
           </NavItem>
           <NavItem eventKey="executive meeting">
             <NavIcon>
-              <i className="fa fa-fw fa-line-chart" style={{ fontSize: "1.75em" }} />
+              <DonutLarge style={{ fontSize: "1.1em", color: "orange" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>Executive Meeting</NavText>
           </NavItem>
           <NavItem eventKey="helpdesk redesign">
             <NavIcon>
-              <i className="fa fa-fw fa-line-chart" style={{ fontSize: "1.75em" }} />
+              <DonutLarge style={{ fontSize: "1.1em", color: "red" }} />
             </NavIcon>
             <NavText style={{ color: "#7F96BA" }}>HelpDesk Redesign</NavText>
           </NavItem>
