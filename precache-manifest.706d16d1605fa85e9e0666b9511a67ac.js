c711440565ed99f0962e72c6b4c3fbe24c586475
Elaine Lee <elaine.lee@ff0000.com>
2019-06-04 15:55:52 -0700

    Updates

---
+++ precache-manifest.706d16d1605fa85e9e0666b9511a67ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645c96dedbd4deba2189c98cfb751a97",
+    "url": "/dashboard-project/index.html"
+  },
+  {
+    "revision": "6f8d988cc677788ddbe3",
+    "url": "/dashboard-project/static/css/2.6749d01e.chunk.css"
+  },
+  {
+    "revision": "829c82b06bd94573ec4c",
+    "url": "/dashboard-project/static/css/main.04c37e74.chunk.css"
+  },
+  {
+    "revision": "6f8d988cc677788ddbe3",
+    "url": "/dashboard-project/static/js/2.14e4bb1f.chunk.js"
+  },
+  {
+    "revision": "829c82b06bd94573ec4c",
+    "url": "/dashboard-project/static/js/main.344afb6a.chunk.js"
+  },
+  {
+    "revision": "a6f900aaf81403438722",
+    "url": "/dashboard-project/static/js/runtime~main.15ba4c05.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/elaineleeworld/dashboard-project/precache-manifest.023cfde32530f6cce5a7c1f343d736df.js"
+  "/dashboard-project/precache-manifest.706d16d1605fa85e9e0666b9511a67ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/elaineleeworld/dashboard-project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dashboard-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
