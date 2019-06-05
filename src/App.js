8f57fb389425451206c9454e76967f9e1b7746d5
Elaine Lee <elaine.lee@ff0000.com>
2019-06-05 15:57:07 -0700

    change color sidenav click element

--- src/App.js
+++ src/App.js
@@ -27,15 +27,26 @@ class App extends Component {
     super(props);
 
     this.state = {
-      clicked: false
+      active: null
     };
   }
 
-  toggleColor = () => {
+  toggleColor = position => {
     console.log("clicked");
-    // if (event.target === "sidenav---navitem") {
-    //   event.target.style.backgroundColor = "#fff";
-    // }
+    if (this.state.active === position) {
+      this.setState({ active: null });
+    } else {
+      this.setState({
+        active: position
+      });
+    }
+  };
+
+  myColor = position => {
+    if (this.state.active === position) {
+      return "#fff";
+    }
+    return "#1F242D";
   };
 
   render() {
@@ -50,100 +61,111 @@ class App extends Component {
         >
           <SideNav.Toggle />
           <SideNav.Nav eventKey="calendar">
-            <NavItem eventKey="calendar" onClick={() => this.toggleColor()}>
+            <NavItem eventKey="calendar" style={{ background: this.myColor(0) }} onClick={() => this.toggleColor(0)}>
               <NavIcon>
                 <CalendarToday style={{ fontSize: "1em", color: "#29A5D8" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>Calendar</NavText>
             </NavItem>
-            <NavItem
-              eventKey="documentation"
-              // style={{ backgroundColor: clicked ? "#fff" : "#1F242D" }}
-              // // onClick={event => this.toggleColor(event.target)}
-              // onClick={event => this.toggleColor(event)}
-            >
+            <NavItem eventKey="documentation" style={{ background: this.myColor(1) }} onClick={() => this.toggleColor(1)}>
               <NavIcon>
                 <Description style={{ fontSize: "1.1em", color: "#29A5D8" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>Documentation</NavText>
             </NavItem>
-            <NavItem
-              eventKey="dashboard"
-              // style={{ backgroundColor: clicked ? "#fff" : "#1F242D" }}
-              // onClick={event => this.toggleColor(event.target)}
-            >
+            <NavItem eventKey="dashboard" style={{ background: this.myColor(2) }} onClick={() => this.toggleColor(2)}>
               <NavIcon>
                 <Home style={{ fontSize: "1.1em", color: "#29A5D8" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>Dashboard</NavText>
             </NavItem>
-            <NavItem eventKey="admin plugins" s>
+            <NavItem eventKey="admin plugins" style={{ background: this.myColor(3) }} onClick={() => this.toggleColor(3)}>
               <NavIcon>
                 <Whatshot style={{ fontSize: "1.1em", color: "#29A5D8" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>Admin Plugins</NavText>
             </NavItem>
-            <NavItem eventKey="admin forms">
+            <NavItem eventKey="admin forms" style={{ background: this.myColor(4) }} onClick={() => this.toggleColor(4)}>
               <NavIcon>
                 <Beenhere style={{ fontSize: "1.1em", color: "#29A5D8" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>Admin Forms</NavText>
             </NavItem>
-            <NavItem eventKey="admin layouts">
+            <NavItem eventKey="admin layouts" style={{ background: this.myColor(5) }} onClick={() => this.toggleColor(5)}>
               <NavIcon>
                 <ViewCompact style={{ fontSize: "1.1em", color: "#29A5D8" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>Admin Layouts</NavText>
             </NavItem>
-            <NavItem eventKey="information panels">
+            <NavItem eventKey="information panels" style={{ background: this.myColor(6) }} onClick={() => this.toggleColor(6)}>
               <NavIcon>
                 <InsertChartOutlined style={{ fontSize: "1.1em", color: "#29A5D8" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>Information Panels</NavText>
             </NavItem>
-            <NavItem eventKey="ecommerce">
+            <NavItem eventKey="ecommerce" style={{ background: this.myColor(7) }} onClick={() => this.toggleColor(7)}>
               <NavIcon>
                 <AttachMoney style={{ fontSize: "1.1em", color: "#29A5D8" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>Ecommerce</NavText>
             </NavItem>
-            <NavItem eventKey="ui elements">
+            <NavItem eventKey="ui elements" style={{ background: this.myColor(8) }} onClick={() => this.toggleColor(8)}>
               <NavIcon>
                 <Memory style={{ fontSize: "1.1em", color: "#29A5D8" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>UI Elements</NavText>
             </NavItem>
-            <NavItem eventKey="form elements">
+            <NavItem eventKey="form elements" style={{ background: this.myColor(9) }} onClick={() => this.toggleColor(9)}>
               <NavIcon>
                 <ViewHeadline style={{ fontSize: "1.1em", color: "#29A5D8" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>Form Elements</NavText>
             </NavItem>
-            <NavItem eventKey="plugins">
+            <NavItem eventKey="plugins" style={{ background: this.myColor(10) }} onClick={() => this.toggleColor(10)}>
               <NavIcon>
                 <DataUsage style={{ fontSize: "1.1em", color: "#29A5D8" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>Plugins</NavText>
             </NavItem>
-            <NavItem eventKey="pages" style={{ backgroundColor: "#1F242D" }}>
+            <NavItem
+              eventKey="pages"
+              style={{ backgroundColor: "#1F242D" }}
+              style={{ background: this.myColor(11) }}
+              onClick={() => this.toggleColor(11)}
+            >
               <NavIcon>
                 <Note style={{ fontSize: "1.1em", color: "#29A5D8" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>Pages</NavText>
             </NavItem>
-            <NavItem eventKey="executive meeting" style={{ backgroundColor: "#1F242D" }}>
+            <NavItem
+              eventKey="executive meeting"
+              style={{ backgroundColor: "#1F242D" }}
+              style={{ background: this.myColor(12) }}
+              onClick={() => this.toggleColor(12)}
+            >
               <NavIcon>
                 <DonutLarge style={{ fontSize: "1.1em", color: "orange" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>Executive Meeting</NavText>
             </NavItem>
-            <NavItem eventKey="helpdesk redesign" style={{ backgroundColor: "#1F242D" }}>
+            <NavItem
+              eventKey="helpdesk redesign"
+              style={{ backgroundColor: "#1F242D" }}
+              style={{ background: this.myColor(13) }}
+              onClick={() => this.toggleColor(13)}
+            >
               <NavIcon>
                 <DonutLarge style={{ fontSize: "1.1em", color: "red" }} />
               </NavIcon>
               <NavText style={{ color: "#7F96BA" }}>HelpDesk Redesign</NavText>
             </NavItem>
-            <NavItem eventKey="sony board meeting" style={{ backgroundColor: "#1F242D" }}>
+            <NavItem
+              eventKey="sony board meeting"
+              style={{ backgroundColor: "#1F242D" }}
+              style={{ background: this.myColor(14) }}
+              onClick={() => this.toggleColor(14)}
+            >
               <NavIcon>
                 <DonutLarge style={{ fontSize: "1.1em", color: "purple" }} />
               </NavIcon>
